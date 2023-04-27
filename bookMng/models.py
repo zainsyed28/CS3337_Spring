16d8e9859bba5b41ea2d64ef1c228a5dfa5c74fe
zainsyed28 <78892882+zainsyed28@users.noreply.github.com>
2023-04-26 21:40:24 -0700

    updated the bootstrap

--- bookMng/models.py
+++ bookMng/models.py
@@ -29,7 +29,7 @@ class Book(models.Model):
 class Comment(models.Model):
     comment = models.CharField(max_length=200)
     publishdate = models.DateField(auto_now=True)
-    book = models.IntegerField(default=0)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     username = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
