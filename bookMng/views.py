b9348dec2ebca17a5529add8c77b711b9840871f
zainsyed28 <78892882+zainsyed28@users.noreply.github.com>
2023-04-26 18:20:25 -0700

    working on making the comments feature now

--- bookMng/views.py
+++ bookMng/views.py
@@ -75,13 +75,12 @@ class Register(CreateView):
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     book.pic_path = book.picture.url[14:]
-    comments = Comments.objects.all().filter(book=book_id)
+    # comments = Comments.objects.all().filter(book=book_id)
     return render(request,
                   'bookMng/book_detail.html',
                   {
                       'item_list': MainMenu.objects.all(),
                       'book': book,
-                      'comments': comments,
                   }
                   )
 
